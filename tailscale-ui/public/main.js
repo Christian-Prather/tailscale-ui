2a30e82cbe786fafc8d2d8595048db5b4666fe66
Christian Prather <clemsbach@gmail.com>
2023-03-17 17:02:21 -0600

    Checkpoint

--- tailscale-ui/public/main.js
+++ tailscale-ui/public/main.js
@@ -1,12 +1,18 @@
 const { app, BrowserWindow } = require("electron");
-const { menubar } = require('menubar');
+// const { menubar } = require('menubar');
+// const { Menu, Tray, nativeImage } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
+const TrayGenerator = require('../src/TrayGenerator');
 
 
-require("../src/tailscale-api.js")
+require(path.join(__dirname, "../src/tailscale-api.js"));
 require("@electron/remote/main").initialize();
 
 // const mb = menubar({
 //   index: "http://localhost:3000",
+//   icon: path.join(__dirname, '../assets/smiley.png'),
+//   tooltip: "Tailscale UI",
 //   browserWindow: {
 //     width: 250,
 //     height: 250,
@@ -19,11 +25,39 @@ require("@electron/remote/main").initialize();
 // }
 // );
 
+// let tray;
+// function createTray(win) {
+//   // const icon = nativeImage.createFromPath('../assets/icon.png')
+//   const icon =  path.join(__dirname, '../assets/smiley.png');
+
+//   tray = new Tray(icon);
+//   tray.BrowserWindow = win;
+
+//   // const contextMenu = Menu.buildFromTemplate([
+//   //   {
+//   //     label: 'Quit',
+//   //     type: 'normal',
+//   //     click() {
+//   //       app.quit();
+//   //     }
+//   //   }
+//   // ]);
+//   tray.setToolTip('Tailscale UI');
+//   tray.setContextMenu(contextMenu);
+// }
+
+
+
 let win = null;
 function createWindow() {
   win = new BrowserWindow({
     width: 800,
     height: 600,
+    show: false,
+    frame: false,
+    fullscreenable: false,
+    resizable: false,
+    skipTaskbar: true,
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true,
@@ -31,10 +65,22 @@ function createWindow() {
     },
   });
 
+  // win.loadURL(
+  //   isDev
+  //   ? "http://localhost:3000"
+  //   : 'index.html'
+  //   );
+
   win.loadURL("http://localhost:3000");
+
 }
 
-app.on("ready", createWindow);
+app.on("ready", () => {
+  createWindow();
+  // createTray();
+  const Tray = new TrayGenerator(win);
+  Tray.createTray();
+});
 
 app.on("window-all-closed", function () {
   if (process.platform !== "darwin") {
@@ -46,8 +92,11 @@ app.on("activate", function () {
   if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
 
-
 // mb.on('ready', () => {
 //   console.log('app is ready');
 //   // your app code here
+// });
+
+// mb.on('after-create-window', () => {
+//   mb.window.loadURL("http://localhost:3000");
 // });
