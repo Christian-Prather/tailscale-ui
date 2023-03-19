765726f5daa3631ef7d96052e01284ddaf5e72dc
Christian Prather <clemsbach@gmail.com>
2023-03-18 19:36:56 -0600

    Squash

--- tailscale-ui/src/exit-node-select.js
+++ tailscale-ui/src/exit-node-select.js
@@ -3,39 +3,36 @@ import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+// import Select from '@mui/material/Select';
+
+import Select from 'react-select';
 
 export default function BasicSelect(props) {
-    const [ip, setIp] = React.useState('');
+    const [ip, setIp] = React.useState('local');
 
     const handleChange = (event) => {
         // setIp(event.target.value);
-        setIp(event.Target.value);
+        setIp(event.target.label);
     };
 
-    const ips = [
-        '10.1.01.100',
-        '101001010101'
-    ]
+    var ips = [
+        { value: 'one', label: 'One' },
+        { value: 'two', label: 'Two' }
+    ];
 
     return (
-        <Box sx={{ minWidth: 120 }}>
-            <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Age</InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={ip}
-                    label="Age"
-                    onChange={handleChange}
-                >
-                    {/* <MenuItem value={10}>Ten</MenuItem>
+        <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={ip}
+            label="Exit Node"
+            onChange={handleChange}
+            options={ips}
+        >
+            {/* <MenuItem value={10}>Ten</MenuItem>
                     <MenuItem value={20}>Twenty</MenuItem>
                     <MenuItem value={30}>Thirty</MenuItem> */}
 
-                    options=ips
-                </Select>
-            </FormControl>
-        </Box>
+        </Select>
     );
 }
