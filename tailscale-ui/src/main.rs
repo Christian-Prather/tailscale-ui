aa8db47b6e681ef9cc83f7391664b6c868600637
Christian Prather <clemsbach@gmail.com>
2024-09-15 19:55:24 -0600

    Added basic slint ui template

--- tailscale-ui/src/main.rs
+++ tailscale-ui/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+
+slint::include_modules!();
+
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>>{
+    let ui = AppWindow::new()?;
+
+    ui.on_request_increase_value({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            ui.set_counter(ui.get_counter() + 1);
+        }
+    });
+    ui.run()?;
+    Ok(())
 }
