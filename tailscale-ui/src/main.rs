c0ab549206fd209e85acd1b945af3d8d823470ba
Christian Prather <clemsbach@gmail.com>
2024-09-18 20:46:51 -0600

    Squash

--- tailscale-ui/src/main.rs
+++ tailscale-ui/src/main.rs
@@ -1,10 +1,19 @@
-
 slint::include_modules!();
 
 use std::error::Error;
 
-fn main() -> Result<(), Box<dyn Error>>{
+fn main() -> Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
+
+    println!("Initializing");
+
+    {
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            ui.set_connection_status(true);
+        }
+    }
 
     ui.on_request_increase_value({
         let ui_handle = ui.as_weak();
@@ -13,6 +22,8 @@ fn main() -> Result<(), Box<dyn Error>>{
             ui.set_counter(ui.get_counter() + 1);
         }
     });
+
     ui.run()?;
+
     Ok(())
 }
